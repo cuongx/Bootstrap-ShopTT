25dafae120895bf3e854cacd44e977518fa2fc9c
cuong <xuanc25@gmail.com>
2020-07-15 09:32:10 +0700

    hightlight

---
+++ js/aindex.js
@@ -0,0 +1,27 @@
+var login = login || { };
+login.check =function (){
+    let email= $("#email").val();
+    let pass=$("#pass").val();
+    let isLogged=false;
+    $.ajax({
+        url:"http://localhost:3000/users",
+        method:"GET",
+        dataType:"json",
+        success: function (data) {
+            $.each(data ,function (i, v) {
+               if(v.username == email && v.password == pass){
+                   isLogged=true;
+                   window.location.href="boosstrap.html"
+               } 
+            });
+            if(!isLogged){
+                $('#error').removeClass('d-none');
+            }
+        }
+    })
+}
+
+$(document).ready(function(){
+        
+});
+
